53fd987709716feae235b81162ed361a3b803d34
Kanitaar <gracembaudeh@gmail.com>
2022-10-21 14:14:43 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * gc: variable text
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1):
+	}
+	else
+	{
+		return (0):
+	}
